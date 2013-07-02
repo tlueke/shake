213622bc3bc4a6db71e428c83c604b6e7bee7e43
tlueke@gmail.com <tlueke@gmail.com>
2013-07-01 20:10:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shake::Application.config.secret_token = 'e7127317f86dfb79c60e6fb46af1a57cf06906533c94ba3453886364f163fb8cdc763150cf136004d2c71ae9cbb014defe06f5aad17b69e10f011b777f82f1ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shake::Application.config.session_store :cookie_store, key: '_shake_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shake::Application.config.session_store :active_record_store
